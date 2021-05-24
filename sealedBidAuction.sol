b2c9d78aad993674d3016b5599d63d351ef7a57e
ionkats <ionkatsara@gmail.com>
2021-05-24 12:29:36 +0300

    1,2verifications, k implementations, zpk+challenge

--- sealedBidAuction.sol
+++ sealedBidAuction.sol
@@ -14,6 +14,7 @@ contract sealedBidAuction{
     uint256 G;
     uint256 H;
     uint16 k = 5; // (times the protocol is implemented) defines the prob of cheating 1/ 2^k 
+    uint counter;
     string public state;
     address auctioneer;
     bool auctioneerReturn;
@@ -54,25 +55,28 @@ contract sealedBidAuction{
         auctioneer = msg.sender;
     }
     
+    
     // assuming the commit phase each participant sends a list of values, xG+rH, w1G+r1H, w2G+r2H, where wi,ri random values for later verification.
     // w1 in [0,q/2], w2 = w1 - q/2, x bid
     function bid(uint256[] memory comm) public payable {
         require((block.timestamp - timeDeployed)<T1, "The commitment period has passed.");
         require(msg.value>F,"Provide at least the appropriate amount.");
-        require(listOfBidders.length <= N," No more bidders allowed.");
+        require(counter <= N," No more bidders allowed.");
         ledger[msg.sender] = msg.value - F;
         deposit = deposit + F;
         Biddings storage bidding = bidders[msg.sender];
-        listOfBidders.push(msg.sender);
         bidding.commit = comm;
+        counter +=1;
     }
     
+
     // cipher is the outcome of encrupting (x,r) by the public key of the auctioneer
     function reveal(string memory cipher) public{
         uint256 time = block.timestamp - timeDeployed;
         require((time>T1)&&(time<T2),"It is not the reveal time.");
         require(bidders[msg.sender].commit[0] != 0, "No commitment was placed in your address");
         bidders[msg.sender].ciphertext = cipher;
+        listOfBidders.push(msg.sender);
     }
     
 
@@ -84,65 +88,80 @@ contract sealedBidAuction{
         require((time>T2)&&(time<T3),"It is not the claim the winner time.");
         require(keccak256(bytes(bidders[probWinner].ciphertext)) != keccak256(bytes("")), "Not a bidder."); 
         //also check if x,r are the commit values used by the probWinner.
-        // require x*G +r*H == bidders[probWinner].commit[0] for G,H
-        // for G,H starting given values
+        require(x*G +r*H == bidders[probWinner].commit[0], "The values are not the commit of this bidder"); // for given G,H
         winner = probWinner;
         highestBid = x;
         state = "Challenge1";
     }
 
 
-    function ZPKCommit(address B, uint256[] memory commits) public{
+    // for a bidder Bi a commit is w1,1 , r1,1 , w2,1 , r2,1, ... , w1,k , r1,k ,w2,k , r2,k
+    function ZPKCommit(address Bi, uint256[] memory commits) public{
         require(keccak256(bytes(state))==keccak256("Challenge1"),"Not valid state.");
         uint256 time = block.timestamp - timeDeployed;
         require((time>T2)&&(time<T3),"It is not the right time.");
-        require(keccak256(bytes(bidders[B].ciphertext)) != keccak256(bytes("")), "Not a bidder.");
+        require(keccak256(bytes(bidders[Bi].ciphertext)) != keccak256(bytes("")), "Not a bidder.");
         zpkCommits = commits;
-        challengeBidder = B;
+        challengeBidder = Bi;
         challengeBlockNumber = block.number;
         state = "Verify";
     }
 
 
+    // by knowing the challengeBlockNumber sends the appropriate responses based on the bits and the 
+    // commits he sent along with the values of the bid the challengeBidder, response is a list with k items.
     function ZPKVerify(uint256[] memory response) public{
         require(keccak256(bytes(state))==keccak256("Verify"), "Not valid state.");
         uint256 time = block.timestamp - timeDeployed;
         require((time>T2)&&(time<T3),"It is not the right time.");
         bytes32 h = keccak256(abi.encodePacked(challengeBlockNumber));
-        for (uint i; i<k; i++){
-            bytes1 b = h[32-i];
+        for (uint j; j<k; j++){
+            bytes1 b = h[32-j];
             if (b==0){
-                // first case verification 
+                require(checkFirstCase(response[j], j), "There is a non verified bidder.");
+                // first case verification Rj = w1j, r1j, w2j, r2j
             }else{
-                // second case verification
+                require(checkSecondCase(response[j], j), "There is a non verified bidder.");
+                // second case verification Rj = (xj+wzj), (uj+rzj), z in{0,1}
             }
         }
-        
         bidders[challengeBidder].validBid = true;
         state = "Challenge2";
     }
-    
+        
 
+    function checkFirstCase(uint256 value, uint j) internal returns(bool){
+        // by value being a list we check the following
+
+        // if (((value[0]*G +value[1]*H) == zpkCommits[j][0]) && ((value[3]*G +value[4]*H) == zpkCommits[j][1])){
+        //     return true;
+        // }
+        // return false;
+    }
+
+
+    function checkSecondCase(uint256 value, uint j) internal returns(bool){
+        // by value being a list we check the following
+
+        // uint256 tempCommit = bidders[challengeBidder].commit[0];
+        // if ((value[0]*G +value[1]*H) == (tempCommit + zpkCommits[j][value[2]]) {
+        //     return true;
+        // }
+        // return false;
+    }
+
+
+    //if the auctioneer keeps someone off the challenge the auction is not continued
     function VerifyAll() public{
         require(keccak256(bytes(state))==keccak256("Challenge2"),"Not valid state.");
         uint256 time = block.timestamp - timeDeployed;
         require((time>T2)&&(time<T3),"It is not the right time.");
         for (uint i; i < listOfBidders.length; i++){
             if (listOfBidders[i] != winner){
-                require(bidders[listOfBidders[i]].validBid == true, "There is a non valid bid.");
+                require(bidders[listOfBidders[i]].validBid == true, "There is a non valid bidder.");
             }
         }
         state = "ValidWinner";
-    }
-    
-
-    function checkFirstCase(uint256 value) internal returns(bool){
-
-    }
-
-
-    function checkSecondCase(uint256 value) internal returns(bool){
-
     }
 
 
@@ -191,3 +210,64 @@ contract sealedBidAuction{
     }
     
 }
+
+
+// THIS CODE IMPLEMENTS THE PROTOCOL ONLY ONCE AND THE AUCTIONEER PROVIDES A LIT OF ALL THE BIDDERS ALONG 
+// WITH ALL THE COMMITS AND LATER ALL THE RESPONSES.
+
+    // // for every bidder i a commit is w1,i , r1,i , w2,i , r2,i
+    // function ZPKCommit(address[] memory B, uint256[] memory commits) public{
+    //     require(keccak256(bytes(state))==keccak256("Challenge1"),"Not valid state.");
+    //     uint256 time = block.timestamp - timeDeployed;
+    //     require((time>T2)&&(time<T3),"It is not the right time.");
+    //     for (uint i; i<B.length;i++){
+    //         require(keccak256(bytes(bidders[B[i]].ciphertext)) != keccak256(bytes("")), "Not a bidder.");
+    //     }
+    //     zpkCommits = commits;
+    //     challengeBidder = B;
+    //     challengeBlockNumber = block.number;
+    //     state = "Verify";
+    // }
+
+
+    // // by knowing the challengeBlockNumber sends the appropriate responses based on the bits and the 
+    // // commits he sent aling with the values of the bid the bidders opened for auctioneer
+    // function ZPKVerify(uint256[] memory response) public{
+    //     require(keccak256(bytes(state))==keccak256("Verify"), "Not valid state.");
+    //     uint256 time = block.timestamp - timeDeployed;
+    //     require((time>T2)&&(time<T3),"It is not the right time.");
+    //     bytes32 h = keccak256(abi.encodePacked(challengeBlockNumber));
+    //     for (uint i; i<challengeBidder.length; i++){
+    //         bytes1 b = h[32-i];
+    //         if (b==0){
+    //             require(checkFirstCase(response[i], i), "There is a non verified bidder.");
+    //             // first case verification Rj = w1j, r1j, w2j, r2j
+    //         }else{
+    //             require(checkSecondCase(response[i], i), "There is a non verified bidder.");
+    //             // second case verification Rj = (xj+wzj), (uj+rzj), z in{0,1}
+    //         }
+    //         bidders[challengeBidder[i]].validBid = true;
+    //     }
+    //     state = "Challenge2";
+    // }
+        
+
+    // function checkFirstCase(uint256 value, uint i) internal returns(bool){
+    //     // by value being a list we check the following
+
+    //     // if (((value[0]*G +value[1]*H) == zpkCommits[i][0]) && ((value[3]*G +value[4]*H) == zpkCommits[i][1])){
+    //     //     return true;
+    //     // }
+    //     // return false;
+    // }
+
+
+    // function checkSecondCase(uint256 value, uint i) internal returns(bool){
+    //     // by value being a list we check the following
+
+    //     // uint256 tempCommit = bidders[challengeBidder[i]].commit[0];
+    //     // if ((value[0]*G +value[1]*H) == (tempCommit + zpkCommits[i][value[2]]) {
+    //     //     return true;
+    //     // }
+    //     // return false;
+    // }
